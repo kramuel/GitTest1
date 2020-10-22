5e6aecc59ebf448b9c190615a7a3aa524f52e757
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-22 09:42:57 +0200

    "how are you" writeline

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitTest1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Samuel!");
+            Console.WriteLine("how are you today?");
 
             Console.ReadKey();
         }
